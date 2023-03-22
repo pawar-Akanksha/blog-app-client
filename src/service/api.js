ffcd717b7335a12daa5da4713144c1dc69c420f3
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-22 07:29:59 -0700

    added files

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
-import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
+import { getAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = 'https://blogapp-5ein.onrender.com';
+const API_URL = 'https://blogappdevelope.onrender.com';
 // const API_URL = "http://localhost:8000";
 const axiosInstance = axios.create({
     baseURL: API_URL,
